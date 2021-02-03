ce8c86ef18cc47d9dadfe7d4f113363c5ce13cbf
amercador <amercador@hotmail.com>
2021-02-02 21:17:34 -0300

    Main view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,37 @@
 package application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			
+// instanciamos um obj loader do tipo FXMLLoader passando o caminho da View;			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+// chamamos o load para carregar a view;			
+			Parent parent = loader.load();
+/* Vamos criar um obj do tipo Scene que será a cena principal e já instaciamos a ...
+cena com o obj principal da view (que é um AnchorPane vazio);*/ 			
+			Scene mainScene = new Scene(parent);
+// chamamos o palco da cena que veio no parametro do método setando a cena dele com cena principal;
+			primaryStage.setScene(mainScene);
+// definimos um título para o palco;
+			primaryStage.setTitle("Sample JavaFX application");
+// mostramos o palco;			
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
