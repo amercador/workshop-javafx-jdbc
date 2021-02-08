c1ecd2e7624f162e8f056341d559260edd391354
amercador <amercador@hotmail.com>
2021-02-08 11:58:14 -0300

    DepartmentForm (dialog) design

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -2,10 +2,10 @@ package model.entities;
 
 import java.io.Serializable;
 
-public class Department implements Serializable{
+public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 	
@@ -20,16 +20,19 @@ public class Department implements Serializable{
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -59,5 +62,4 @@ public class Department implements Serializable{
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-	
 }
